493e85d4755c1a816efeaad82ffba8c646d454f6
梶塚太智 <kajizukataichi@outlook.jp>
2024-07-21 17:55:15 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::collections::HashMap;
+use std::io::{self, Write};
+
+struct AmaLog {
+    logics: HashMap<String, String>,
+}
+
+impl AmaLog {
+    fn new() -> AmaLog {
+        AmaLog {
+            logics: HashMap::new(),
+        }
+    }
+
+    fn set_fact(&mut self, subject: String, object: String) {
+        self.logics
+            .entry(subject)
+            .and_modify(|value| *value = object.clone())
+            .or_insert(object);
+    }
+
+    fn query(&mut self, subject: String, object: String) -> bool {
+        let result = self.logics.get(&subject);
+        if let Some(i) = result {
+            if *i == object {
+                true
+            } else {
+                self.query(i.clone(), object)
+            }
+        } else {
+            false
+        }
+    }
+}
+
+/// Get standard input
+fn input(prompt: &str) -> String {
+    print!("{}", prompt);
+    io::stdout().flush().unwrap();
+    let mut result = String::new();
+    io::stdin().read_line(&mut result).ok();
+    result.trim().to_string()
+}
+
+fn main() {
+    let mut amalog = AmaLog::new();
+    loop {
+        let program = input("> ");
+        let chars = program.chars().collect::<Vec<char>>();
+        match chars.get(0).unwrap_or(&' ') {
+            '!' => {
+                let program = &program[1..chars.len()];
+                let program = program.split("=").collect::<Vec<&str>>();
+                let (subject, object) = (program[0].trim(), program[1].trim());
+                amalog.set_fact(subject.to_string(), object.to_string());
+                println!("{:?}", amalog.logics)
+            }
+            '?' => {
+                let program = &program[1..chars.len()];
+                let program = program.split("=").collect::<Vec<&str>>();
+                let (subject, object) = (program[0].trim(), program[1].trim());
+                println!("{}", amalog.query(subject.to_string(), object.to_string()));
+            }
+            _ => {}
+        }
+    }
+}
